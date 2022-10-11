2586798baf6db95c943e52a5842d0460619872ea
Oswin Ma <oswin.ma@gmail.com>
2022-10-10 23:59:47 -0400

    fix issues

--- mariadb/sql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `tsaheylu_me` /*!40100 DEFAULT CHARACTER SET utf8 */;
-create user 'springuser'@'%' identified by 'ThePassword';
-grant all on tsaheylu_me.* to 'springuser'@'%';
-flush privileges;
-use tsaheylu_me;
-
-
-
